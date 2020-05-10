580458c1b2be5dda7ef3af6b1cf542545cd1c752
emin <samburer200@gmail.com>
2020-05-10 16:21:08 +0300

    add validation for signUp

--- src/main/java/ru/itis/biology/controllers/SignUpController.java
+++ src/main/java/ru/itis/biology/controllers/SignUpController.java
@@ -3,10 +3,14 @@ package ru.itis.biology.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import ru.itis.biology.dto.SignUpDto;
 import ru.itis.biology.service.SignUpService;
+
+import javax.validation.Valid;
 
 
 @Controller
@@ -17,16 +21,20 @@ public class SignUpController {
 
 
     @GetMapping("/signUp")
-    public String getSignUpPage(Authentication authentication) {
+    public String getSignUpPage(Authentication authentication, Model model) {
         if (authentication != null) {
             return "redirect:/profile";
         }
+        model.addAttribute("form", new SignUpDto());
         return "sign_up";
     }
 
     @PostMapping("/signUp")
-    public String signUp(SignUpDto form) {
+    public String signUp(@Valid SignUpDto form, BindingResult bindingResult, Model model) {
+        System.out.println(form);
         service.signUp(form);
+        System.out.println(bindingResult.getAllErrors());
+        model.addAttribute("form", form);
         return "redirect:/";
     }
 }
--- src/main/java/ru/itis/biology/dto/SignUpDto.java
+++ src/main/java/ru/itis/biology/dto/SignUpDto.java
@@ -2,14 +2,38 @@ package ru.itis.biology.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
 @Data
 public class SignUpDto {
+
+    @NotNull(message = "Напишите свое имя")
+    @Pattern(regexp = "[А-Яа-я]+", message = "Неправильное имя")
     private String name;
+
+    @NotNull(message = "Напишите свою фамилию")
+    @Pattern(regexp = "[А-Яа-я]+", message = "Неправильная фамилия")
     private String fullname;
+
+    @NotNull(message = "Неправильный возраст")
     private Integer age;
+
+    @NotNull(message = "Неправильный класс")
     private Integer classNumber;
+
+    @NotNull(message = "Напишите свое email")
+    @Email(message = "Неправильный email")
     private String email;
+
+    @NotNull(message = "Напишите свой пароль")
     private String password;
+
+    @NotNull(message = "Напишите свой номер телефон")
+    @Pattern(regexp = "^\\+\\d{2}\\(\\d{3}\\)\\d{3}-\\d{2}-\\d{2}$", message = "Неправильный формат номера телефона")
     private String phone;
+
+    @NotNull(message = "Напишите свою роль")
     private String role;
 }
